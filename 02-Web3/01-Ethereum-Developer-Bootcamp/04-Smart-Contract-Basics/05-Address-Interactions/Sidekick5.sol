c89b238039de84b6104192cd368827189f2c2620
TylerHYS <tylerhys98@gmail.com>
2023-11-10 16:30:58 +0800

    .

---
+++ 02-Web3/01-Ethereum-Developer-Bootcamp/04-Smart-Contract-Basics/05-Address-Interactions/Sidekick5.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Sidekick {
+    function makeContact(address hero) external {
+        (bool success, ) = hero.call(abi.encodeWithSignature("alert(uint,bool)", 10, true));
+
+        require(success);
+    }
+}
